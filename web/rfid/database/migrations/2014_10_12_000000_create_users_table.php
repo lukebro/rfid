5f51be704ffd2c9f4dd949b3d8fc1ee83e793c30
edmiralagic <edmir.alagic@my.ccsu.edu>
2017-03-29 13:23:31 -0400

    Migrations fixed

--- web/rfid/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/rfid/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('username');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('username');
+                $table->string('email');
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
