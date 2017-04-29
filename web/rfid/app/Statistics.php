61787abec6edd93e22ca2c5ade49a7ede6396e8d
edmiralagic <edmir.alagic@my.ccsu.edu>
2017-04-28 22:41:00 -0400

    Changed to stats

--- web/rfid/app/Statistics.php
+++ web/rfid/app/Statistics.php
@@ -33,6 +33,8 @@ class Statistics
 		17 => [],
 		18 => [],
 		19 => [],
+		20 => [],
+		21 => [],
 	];
 
 	public function __construct()
@@ -90,6 +92,56 @@ class Statistics
 		});
 
 		dd($hoursTotal);
+	}
+
+	public function validateCapture($capture)
+	{
+		$hour = $capture->created_at->hour;
+
+		if ($hour < 8) {
+
+			return false;
+		}
+		if ($hour > 21) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	public function sameDay($date1, $date2) {
+		if($date1->day != $date2->day) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	public function avgTimeSpent()
+	{
+		$count = 0;
+		$totalDiff = 0;
+		$avg = Carbon::now();
+
+		$all = $this->all();
+		$userGroups = $all->groupBy('card');
+
+
+		foreach($userGroups as $item) {
+    		for($i = sizeof($item) - 1; $i > 0; $i = $i - 2) {
+    			if($item[$i] != null && $item[$i - 1] != null){
+    				if($this->sameDay($item[$i]->created_at, $item[$i - 1]->created_at)) {
+    					$totalDiff += abs($item[$i]->created_at->diffInMinutes($item[$i - 1]->created_at));
+    					$count++;
+    				}
+    			}
+    		}
+		}
+
+		$avg = ($totalDiff/$count);
+		dd($avg);
 	}
 
 
